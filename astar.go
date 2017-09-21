833749ab56c674ccb962d7f841cafab7d94ad9bb
Andreas T Jonsson <mail@andreasjonsson.se>
2017-09-21 21:39:47 +0200

    Added a more flexible interface.

--- astar.go
+++ astar.go
@@ -9,12 +9,12 @@ import "container/heap"
 type Pather interface {
 	// PathNeighbors returns the direct neighboring nodes of this node which
 	// can be pathed to.
-	PathNeighbors() []Pather
+	PathNeighbors(ctx interface{}, neighbors []Pather) []Pather
 	// PathNeighbourCost calculates the exact movement cost to neighbor nodes.
-	PathNeighborCost(to Pather) float64
+	PathNeighborCost(ctx interface{}, to Pather) float64
 	// PathEstimatedCost is a heuristic method for estimating movement costs
 	// between non-adjacent nodes.
-	PathEstimatedCost(to Pather) float64
+	PathEstimatedCost(ctx interface{}, to Pather) float64
 }
 
 // node is a wrapper to store A* data for a Pather node.
@@ -43,54 +43,113 @@ func (nm nodeMap) get(p Pather) *node {
 	return n
 }
 
-// Path calculates a short path and the distance between the two Pather nodes.
-//
+type result struct {
+	path        []Pather
+	distance    float64
+	found, done bool
+}
+
+// Search is an object representing the current search state.
+type Search struct {
+	nm       nodeMap
+	nq       *priorityQueue
+	tmp      []Pather
+	fromNode *node
+	to       Pather
+	ctx      interface{}
+	res      result
+}
+
+// NewSearch creates a new search object.
+func NewSearch(ctx interface{}, from, to Pather) *Search {
+	s := &Search{}
+	s.nm = nodeMap{}
+	s.nq = &priorityQueue{}
+	s.tmp = make([]Pather, 0, 8)
+	heap.Init(s.nq)
+	s.fromNode = s.nm.get(from)
+	s.fromNode.open = true
+	heap.Push(s.nq, s.fromNode)
+	s.to = to
+	s.ctx = ctx
+	return s
+}
+
+// PathWithContext calculates a short path and the distance between the two Pather nodes.
+// ctx is user optional data.
 // If no path is found, found will be false.
+func PathWithContext(ctx interface{}, from, to Pather) (path []Pather, distance float64, found bool) {
+	s := NewSearch(ctx, from, to)
+	for !s.Step() {
+	}
+	return s.Result()
+}
+
+// Path calculates a short path and the distance between the two Pather nodes.
 func Path(from, to Pather) (path []Pather, distance float64, found bool) {
-	nm := nodeMap{}
-	nq := &priorityQueue{}
-	heap.Init(nq)
-	fromNode := nm.get(from)
-	fromNode.open = true
-	heap.Push(nq, fromNode)
-	for {
-		if nq.Len() == 0 {
-			// There's no path, return found false.
-			return
+	return PathWithContext(nil, from, to)
+}
+
+// Result retrives the final search result.
+func (s *Search) Result() (path []Pather, distance float64, found bool) {
+	for !s.Step() {
+	}
+	return s.res.path, s.res.distance, s.res.found
+}
+
+// Step advances the search.
+//
+// Returns true if the search is done.
+func (s *Search) Step() bool {
+	if s.res.done || s.nq.Len() == 0 {
+		// There's no path or we are already done.
+		s.res.done = true
+		return s.res.done
+	}
+
+	current := heap.Pop(s.nq).(*node)
+	current.open = false
+	current.closed = true
+
+	if current == s.nm.get(s.to) {
+		// Found a path to the goal.
+		p := []Pather{}
+		curr := current
+		for curr != nil {
+			p = append(p, curr.pather)
+			curr = curr.parent
 		}
-		current := heap.Pop(nq).(*node)
-		current.open = false
-		current.closed = true
 
-		if current == nm.get(to) {
-			// Found a path to the goal.
-			p := []Pather{}
-			curr := current
-			for curr != nil {
-				p = append(p, curr.pather)
-				curr = curr.parent
+		s.res.path = p
+		s.res.distance = current.cost
+		s.res.found = true
+
+		return true
+	}
+
+	for _, neighbor := range current.pather.PathNeighbors(s.ctx, s.tmp[:0]) {
+		cost := current.cost + current.pather.PathNeighborCost(s.ctx, neighbor)
+		neighborNode := s.nm.get(neighbor)
+		if cost < neighborNode.cost {
+			if neighborNode.open {
+				heap.Remove(s.nq, neighborNode.index)
 			}
-			return p, current.cost, true
+			neighborNode.open = false
+			neighborNode.closed = false
 		}
-
-
-		for _, neighbor := range current.pather.PathNeighbors() {
-			cost := current.cost + current.pather.PathNeighborCost(neighbor)
-			neighborNode := nm.get(neighbor)
-			if cost < neighborNode.cost {
-				if neighborNode.open {
-					heap.Remove(nq, neighborNode.index)
-				}
-				neighborNode.open = false
-				neighborNode.closed = false
-			}
-			if !neighborNode.open && !neighborNode.closed {
-				neighborNode.cost = cost
-				neighborNode.open = true
-				neighborNode.rank = cost + neighbor.PathEstimatedCost(to)
-				neighborNode.parent = current
-				heap.Push(nq, neighborNode)
-			}
+		if !neighborNode.open && !neighborNode.closed {
+			neighborNode.cost = cost
+			neighborNode.open = true
+			neighborNode.rank = cost + neighbor.PathEstimatedCost(s.ctx, s.to)
+			neighborNode.parent = current
+			heap.Push(s.nq, neighborNode)
 		}
 	}
+
+	return false
+}
+
+// Context returns the users search context.
+func (s *Search) Context() interface{} {
+	return s.ctx
 }
